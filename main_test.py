e1fa9c0962cf5fb663e031896a99212de65ccfd1
Victor Roy <victor.a.roy@gmail.com>
2016-06-10 03:04:56 -0400

    Change dtype to Categorical, some Feature Engineering

--- main_test.py
+++ main_test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 """
 Created on Tue May 31 16:24:59 2016
 
@@ -18,7 +18,7 @@ def importData():
     print "Importing data from train.csv..."    
     
     
-    dtype_dict = {'date_time':str, 
+    dtype_dict = {'date_time':np.datetime64, 
                    'site_name': np.int32, 
                    'posa_continent':np.int32, 
                    'user_location_country':np.int32, 
@@ -29,8 +29,8 @@ def importData():
                    'is_mobile':np.bool_, 
                    'is_package':np.bool_, 
                    'channel':np.int32, 
-                   'srch_ci':str, 
-                   'srch_co':str, 
+                   'srch_ci':np.datetime64, 
+                   'srch_co':np.datetime64, 
                    'srch_adults_cnt':np.int32, 
                    'srch_children_cnt':np.int32, 
                    'srch_rm_cnt':np.int32, 
@@ -49,6 +49,15 @@ def importData():
 
     train = pd.read_csv('train.csv', dtype=dtype_dict, parse_dates= ['date_time', 'srch_ci','srch_co'], nrows=n_rows)
     
+    cat_vars = ['site_name','posa_continent','user_location_country', 'user_location_region', 'user_location_city', 'is_mobile', 'is_package', 'channel', 'srch_destination_type_id', 'hotel_continent', 'hotel_country', 'hotel_market', 'hotel_cluster']
+    ordered_cat_vars = ['srch_adults_cnt', 'srch_children_cnt', 'srch_rm_cnt', 'is_booking']
+    
+    for i in cat_vars:
+        train[i] = train[i].astype("category", ordered=False)
+    for i in ordered_cat_vars:
+        train[i] = train[i].astype("category", ordered=True)
+    
+    
     end_time = time.time()
     
     duration = end_time-start_time
@@ -59,31 +68,40 @@ def importData():
     
     
 def featureEngineering(train):
-     
-     
-    # Calc # nights
-    num_nights = train['srch_co']-train['srch_ci']
-    num_nights = (num_nights/np.timedelta64(1,'D')).astype(float)
-    train['num_nights'] = num_nights
+    # Number of days searching in advance of check-in
+    train['days_advance'] = pd.to_datetime(train['srch_ci'], errors='coerce') - pd.to_datetime(train['date_time'],errors='coerce')
     
-    # Calc # days searching in advance
-    days_advance = (train['srch_ci']-train['date_time'])/np.timedelta64(1,'D')
-    train['days_advance'] = days_advance
+    # Number of nights of stay searched for, round to day.
+    train['num_nights'] = pd.to_datetime(train['srch_co'], errors = 'coerce')-pd.to_datetime(train['srch_ci'], errors = 'coerce')
+    train['num_nights'] = train['num_nights'].astype('category',ordered = False)
     
-# unfinished
-def calcPopular(train):
-    train.groupby(('srch_destination_id','is_booking'))    
+    # Day of week; 0=Monday, 6=Sunday
+    train['day_of_week'] = pd.to_datetime(train['date_time'], errors='coerce').dt.dayofweek
+    train['day_of_week'] = train['day_of_week'].astype('category', ordered = False)
+
+    # Month in which search-check-in day resides; 0 for January, 11 for December
+    train['srch_ci_month'] = pd.to_datetime(train['srch_ci'], errors='coerce').dt.month
+
+    # True for winter/summer; False for autumn/spring    
+    # Winter/Summer counts as [0,1,11,10,5,6,7,8], where 0 = Jan; 11 = Dec
+    train['season'] = train['srch_ci_month'].map(lambda x: x in [0,1,11,10,5,6,7,8])
     
+    
+    return train
     
 
 def main():
     
-    train = importData()
+    # Store a quick pickle for ease of testing in console
+    #train = importData()
+    train = pd.read_pickle('train.pickle')
     
     train = featureEngineering(train)
-
-    #most_popular = calcPopular(train)     
     
+    train.to_pickle('train.pickle')
+   
+
+main()
     
     
     
